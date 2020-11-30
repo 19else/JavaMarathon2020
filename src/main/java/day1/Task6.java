2648b2a2c84086892dd97adba757c12992e9f52f
19else <159753ElSe->
2020-11-30 18:38:19 +0300

    Complete day 1 tasks

--- src/main/java/day1/Task6.java
+++ src/main/java/day1/Task6.java
@@ -2,6 +2,10 @@ package day1;
 
 public class Task6 {
     public static void main(String[] args) {
-
+        int k = 2;
+        for (int i = 1; i < 10 ; i++) {
+            int rsl = i * k;
+            System.out.println(i + " x " + k + " = " + rsl);
+        }
     }
 }
