02a871dce44d8a0b33084373a873da51675db421
19else <159753ElSe->
2020-12-02 22:13:08 +0300

    Complete day 3 tasks

--- src/main/java/day3/Task2.java
+++ src/main/java/day3/Task2.java
@@ -1,7 +1,20 @@
 package day3;
 
+import java.util.Scanner;
+
 public class Task2 {
     public static void main(String[] args) {
-
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+            double dividend = scanner.nextDouble();
+            double divider = scanner.nextDouble();
+            if (divider != 0) {
+                System.out.println(dividend / divider);
+            }
+            if (divider == 0) {
+                System.out.println("работа программы завершается");
+                break;
+            }
+        }
     }
 }
--- src/main/java/day3/Task3.java
+++ src/main/java/day3/Task3.java
@@ -1,7 +1,22 @@
 package day3;
 
+import java.util.Scanner;
+
 public class Task3 {
     public static void main(String[] args) {
-
+        Scanner scanner = new Scanner(System.in);
+        int count = 0;
+      while (count < 5) {
+            double dividend = scanner.nextDouble();
+            double divider = scanner.nextDouble();
+            if (divider != 0) {
+                System.out.println(dividend / divider);
+                count++;
+            }
+            if (divider == 0) {
+                System.out.println("Деление на 0");
+            }
+          //  System.out.println("count = " + count);
+        }
     }
 }
