410bbfdc7a864bc4fadccac895ed1abfd2f7642b
19else <159753ElSe->
2020-12-07 15:25:36 +0300

    Complete day 8 Tasks

---
+++ src/main/java/day8/Airplane.java
@@ -0,0 +1,55 @@
+package day8;
+
+public class Airplane {
+    private String producer;
+    private int year;
+    private double length;
+    private double weight;
+    private int fuel;
+
+    Airplane(String producer, int year, int length, int weight) {
+        this.producer = producer;
+        this.year = year;
+        this.length = length;
+        this.weight = weight;
+        this.fuel = 0;
+    }
+
+    public void  info() {
+        System.out.println("Изготовитель: " + producer + " Год выпуска: " + getYear()
+                + " Длина: " + getLength() + " Вес: " + weight + " Количество топлива в баке: " + fuel);
+    }
+
+    public static void compareAirplane(Airplane airplane1, Airplane airplane2) {
+        if (airplane1.length > airplane2.length) {
+            System.out.println(airplane1.producer + " is longer");
+        } else {
+            System.out.println(airplane2.producer + " is longer");
+        }
+    }
+
+    public int fillUp(int refuel) {
+        return this.fuel = refuel;
+    }
+
+    public void setLength(double length) {
+        this.length = length;
+    }
+
+    public double getLength() {
+        return length;
+    }
+
+    public void setYear (int year) {
+        this.year = year;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public String toString() {
+        return "Изготовитель: " + producer + " Год выпуска: " + year +
+                " Длина: " + length + " Вес: " + weight;    }
+
+}
--- src/main/java/day8/Task1.java
+++ src/main/java/day8/Task1.java
@@ -2,6 +2,22 @@ package day8;
 
 public class Task1 {
     public static void main(String[] args) {
+        StringBuilder sb = new StringBuilder();
+        long startTime = System.currentTimeMillis();
+        for (int i = 0; i <= 20000; i++) {
+            System.out.print(i + " ");
+        }
+        long spentTime = System.currentTimeMillis() - startTime;
+        System.out.println();
+        System.out.println("Время затраченное на конкатенацию :  " + spentTime + " мс");
 
+        startTime = System.currentTimeMillis();
+        for (int i = 0; i <= 20000 ; i++) {
+            sb.append(i).append(" ");
+        }
+        spentTime = System.currentTimeMillis() - startTime;
+        System.out.print(sb);
+        System.out.println();
+        System.out.println("Время затраченное StringBuilder:  " + spentTime + " мс");
     }
 }
