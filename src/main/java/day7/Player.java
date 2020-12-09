8b83155da363c437cb1f08842935230aa58b5f4d
19else <159753ElSe->
2020-12-09 12:55:56 +0300

    Complete day 7 Tasks

--- src/main/java/day7/Player.java
+++ src/main/java/day7/Player.java
@@ -1,12 +1,12 @@
 package day7;
 
 public class Player {
-    int stamina;
-    private static final int  MAX_STAMINA = 100;
+    private int stamina;
+    private static final int MAX_STAMINA = 100;
     private static final int MIN_STAMINA = 0;
     private static int countPlayers = 0;
 
-    public Player (int stamina) {
+    public Player(int stamina) {
         this.stamina = stamina;
         if (countPlayers < 6) {
             countPlayers++;
@@ -22,20 +22,20 @@ public class Player {
     }
 
     public void run() {
-
         stamina = stamina - 1;
-        System.out.println("Выносливость = " + stamina);
+        System.out.println("Выносливость игрока = " + stamina);
         if (stamina == MIN_STAMINA) {
             countPlayers = countPlayers - 1;
             System.out.println("Игрок покинул поле");
         } else {
             if (stamina < 0) {
-            System.out.println("Выносливость не может быть меньше 0");
-        }
+                System.out.println("Выносливость не может быть меньше 0");
+                stamina = 0;
+            }
         }
     }
 
-    public void info() {
+    public static void info() {
         if (countPlayers < 6) {
             System.out.println("Команды неполные. На поле еще есть " + (6 - countPlayers) + " свободных мест");
         } else {
