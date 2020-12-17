e400601cf1709f84f7480e1d5e2840bdbe0f6174
19else <159753ElSe->
2020-12-17 10:13:18 +0300

    Complete day 12 Tasks

--- src/main/java/day12/task4/MusicBand.java
+++ src/main/java/day12/task4/MusicBand.java
@@ -10,19 +10,8 @@ public class MusicBand {
     private int year;
     private String userNameSurname;
     private List<String> bands;
-    private List<MusicBand> unionBands;
-    private List<MusicBand> bandA;
 
-    public List<MusicBand> getBandA() {
-        return bandA;
-    }
 
-    public void setBandA(List<MusicBand> bandA) {
-        this.bandA = bandA;
-    }
-
-    public MusicBand() {
-    }
 
     public MusicBand(String name, int year, String userNameSurname, List<String> bands) {
         this.name = name;
@@ -31,25 +20,13 @@ public class MusicBand {
         this.bands = bands;
     }
 
-    public List<MusicBand> getUnionBands() {
-        return unionBands;
-    }
-
-    public void setUnionBands(List<MusicBand> unionBands) {
-        this.unionBands = unionBands;
-    }
-
-    public String getName() { return name; }
+       public String getName() { return name; }
 
     public void setName(String name) { this.name = name; }
 
     public int getYear() { return year; }
 
     public void setYear(int year) { this.year = year; }
-
-    public String getUserNameSurname() { return userNameSurname; }
-
-    public void setUserNameSurname(String userNameSurname) { this.userNameSurname = userNameSurname; }
 
     public List<String> getBands() {
         return bands;
@@ -62,19 +39,15 @@ public class MusicBand {
     public static void transferMembers (MusicBand bandA, MusicBand unionBands) {
         bandA.getBands().addAll(unionBands.getBands());
         unionBands.getBands().clear();
-        System.out.println("Печать из transferMembers = " + bandA);
-
      }
 
     public void printMembers() {
-        System.out.println("printMembers = " + getBands());
+        System.out.println("printMembers = " + bands);
     }
 
-    public String getMembers() {
-        return bands.toString();
+    public void getMembers() {
+        System.out.println(bands);
     }
-
-
 
     @Override
     public String toString() {
--- src/main/java/day12/task4/Task4.java
+++ src/main/java/day12/task4/Task4.java
@@ -1,6 +1,7 @@
 package day12.task4;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class Task4 {
@@ -13,13 +14,12 @@ public class Task4 {
         groupMember2.add("David");
         groupMember2.add("Tommy");
         groupMember2.add("Reb");
+        Collections.shuffle(groupMember);
+        Collections.shuffle(groupMember2);
         MusicBand band1 = new MusicBand("Scorpions", 1965, "Klaus Meine", groupMember);
         MusicBand band = new MusicBand("Whitesnake", 1977, "Michael Devin", groupMember2);
-         MusicBand.transferMembers(band1, band);
-        System.out.println("Список после объединения:" + groupMember);
-        System.out.println("список ушедшей группы: " + groupMember2);
-        System.out.println("void printMember:");
+        MusicBand.transferMembers(band1, band);
         band1.printMembers();
-        System.out.println("getMember : " + band1.getMembers());
+        System.out.println(band1);
     }
 }
